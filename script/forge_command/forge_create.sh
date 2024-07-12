6433d4c262dd21e5e520c0a117004599878c6ed2
chupengfei <450793585@qq.com>
2024-07-12 20:13:28 +0800

    feat: deploy and verify

--- script/forge_command/forge_create.sh
+++ script/forge_command/forge_create.sh
@@ -6,4 +6,4 @@
 # 部署你的合约到测试网
 # forge create src/LouisToken.sol:MyToken --rpc-url sepolia --account mainTestAccount --constructor-args "My Test Token" "MTT" --etherscan-api-key MTPWKWF5E99E1P5C6GH4BIJ69XF8AMEI83
 
-# forge verify-contract 0x0937a7bE8817cbcfF6e83F2F5e4EA188E14Dfd32 src/LouisToken.sol:MyToken --chain 11155111 --rpc-url sepolia --num-of-optimizations 200 --compiler-version v0.8.25+commit.9ef177df --etherscan-api-key MTPWKWF5E99E1P5C6GH4BIJ69XF8AMEI83 --constructor-args $(cast abi-encode "constructor(string,string)" "My Test Token" "MTT")
+forge script --chain sepolia script/LouisToken.s.sol:MyTokenScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
