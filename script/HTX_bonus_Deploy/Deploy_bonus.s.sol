3b4fc494468afd93eebfc9af2db72d5fe3177f1b
chupengfei <450793585@qq.com>
2024-10-12 20:48:44 +0800

    feat: add deploy script

---
+++ script/HTX_bonus_Deploy/Deploy_bonus.s.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import {Script, console} from "forge-std/Script.sol";
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "../../src/HTX-bonus/InviteAndDividendUpgradeable.sol";
+
+contract DeployInviteAndDividendUpgradeable is Script {
+    function run() external {
+        // 从环境变量中获取私钥或在此处定义私钥
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        // 根据私钥生成部署者的地址
+        address deployerAddress = vm.addr(deployerPrivateKey);
+
+        console.log("deployerPrivateKey", deployerPrivateKey);
+
+        // 启动广播以启用交易
+        vm.startBroadcast(deployerPrivateKey);
+
+        // 部署合约逻辑
+        InviteAndDividendUpgradeable logic = new InviteAndDividendUpgradeable();
+
+        // 部署 Proxy 管理合约
+        ProxyAdmin proxyAdmin = new ProxyAdmin(deployerAddress);
+
+        // 初始化参数
+        address usdtToken = 0x55d398326f99059fF775485246999027B3197955; // USDT 代币地址
+        address htxToken = 0x61EC85aB89377db65762E234C946b5c25A56E99e; // HTX 代币地址
+        address trxToken = 0xCE7de646e7208a4Ef112cb6ed5038FA6cC6b12e3; // TRX 代币地址
+        address dexRouter = 0xd0F08FE0B691C6a7b280c8DD5C7bC6D44Ad35e35; // DEX 路由合约地址
+        uint256 oneTimeDividend = 100; // 一次性分红金额
+
+        // 编码初始化函数数据
+        bytes memory data = abi.encodeWithSignature(
+            "initialize(address,address,address,address,uint256)",
+            usdtToken,
+            htxToken,
+            trxToken,
+            dexRouter,
+            oneTimeDividend
+        );
+
+        // 部署 TransparentUpgradeableProxy 并通过 ProxyAdmin 进行管理
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(logic), address(proxyAdmin), data);
+
+        // 停止广播
+        vm.stopBroadcast();
+
+        console.log("Logic contract deployed at:", address(logic));
+        console.log("Proxy contract deployed at:", address(proxy));
+        console.log("Proxy Admin deployed at:", address(proxyAdmin));
+        console.log("ProxyAdmin owner:", proxyAdmin.owner());
+    }
+}
--- src/HTX-bonus/InviteAndDividendUpgradeable.sol
+++ src/HTX-bonus/InviteAndDividendUpgradeable.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-// import {Test, console} from "forge-std/Test.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 interface IDexRouter {
     function swapExactTokensForTokens(
@@ -16,51 +16,60 @@ interface IDexRouter {
     ) external returns (uint256[] memory amounts);
 }
 
-contract InviteAndDividend is Ownable, ReentrancyGuard {
-    IERC20 public BSC_USDT_Token; // 0x55d398326f99059fF775485246999027B3197955
-    IERC20 public BSC_HTX_Token; // 0x61EC85aB89377db65762E234C946b5c25A56E99e
-    IERC20 public BSC_TXR_Token; // 0xCE7de646e7208a4Ef112cb6ed5038FA6cC6b12e3
+contract InviteAndDividendUpgradeable is Initializable, OwnableUpgradeable, ReentrancyGuardUpgradeable {
+    ERC20Upgradeable public BSC_USDT_Token;
+    ERC20Upgradeable public BSC_HTX_Token;
+    ERC20Upgradeable public BSC_TXR_Token;
     IDexRouter public dexRouter;
 
-    uint256 public constant DEPOSIT_AMOUNT = 100 * 1e18; // 用户每次入金的金额是固定的, 为 100 USDT
-    uint256 public constant DIRECT_REWARD_AMOUNT = 38 * 1e18; // 入金的用户，其直接推荐他的上级获得 38 USDT
-    uint256 public constant INDIRECT_REWARD_AMOUNT = 2 * 1e18; // 上级的上级, 获取的推荐奖励为 2 USDT
-    uint256 public constant SWAP_USDT_TO_HTX_AMOUNT = 30 * 1e18; // 用户每入金100 USDT, 将其中的30 USDT通过 Dex 兑换成 HTX Token
+    uint256 public constant DEPOSIT_AMOUNT = 100 * 1e18;
+    uint256 public constant DIRECT_REWARD_AMOUNT = 38 * 1e18;
+    uint256 public constant INDIRECT_REWARD_AMOUNT = 2 * 1e18;
+    uint256 public constant SWAP_USDT_TO_HTX_AMOUNT = 30 * 1e18;
     uint256 public constant SWAP_THRESHOLD = 300 * 1e18;
-    uint256 public constant MAX_TOTAL_REWARD_PER_DEPOSIT = 500 * 1e18; // 单次投注，用户总收益上限为500 USDT
-    uint256 deployTime; // 合约部署的时间戳
-    uint256 public one_time_dividend; // 用户入金HTX分红
+    uint256 public constant MAX_TOTAL_REWARD_PER_DEPOSIT = 500 * 1e18;
+    uint256 public deployTime;
+    uint256 public one_time_dividend;
     uint256 public accumulatedUSDTForSwap;
-    uint256[] public everyDayDividendAmountArr; // 每日分红金额数组，需要管理员进行设置
-    bool public paused = false; // 系统是否暂停投注, 默认为否
+    uint256[] public everyDayDividendAmountArr;
+    bool public paused;
+
+    struct User {
+        address referrer;
+        uint256 directReward;
+        uint256 indirectReward;
+        uint256 totalReward;
+        uint8 depositCount;
+        uint256 lastUpdateTime;
+        bool isBound;
+        bool hasDeposited;
+        address[] referrals;
+    }
 
     mapping(address => User) public users;
 
-    struct User {
-        address referrer; // 用户的推荐人，即推荐他的上级用户
-        uint256 directReward; // 直接推荐奖励
-        uint256 indirectReward; // 间接推荐奖励
-        uint256 totalReward; // 总计推荐奖励, 包含直接推荐和间接推荐
-        uint8 depositCount; // 投注次数
-        uint256 lastUpdateTime; // 最后一次更新分红时间戳
-        bool isBound; // 是否与上级绑过, 同一个地址只允许与一位用户绑定, 作为他/她的上级
-        bool hasDeposited; // 是否有过投注行为, 是否已入金
-        address[] referrals; // 用户所推荐的下级, 这是直接推荐的, 不含所有间接推荐用户
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
     }
 
-    constructor(
+    function initialize(
         address _usdtToken,
         address _htxToken,
         address _trxToken,
         address _dexRouter,
         uint256 _one_time_dividend
-    ) Ownable(msg.sender) {
-        BSC_USDT_Token = IERC20(_usdtToken);
-        BSC_HTX_Token = IERC20(_htxToken);
-        BSC_TXR_Token = IERC20(_trxToken);
+    ) public initializer {
+        __Ownable_init(msg.sender);
+        __ReentrancyGuard_init();
+
+        BSC_USDT_Token = ERC20Upgradeable(_usdtToken);
+        BSC_HTX_Token = ERC20Upgradeable(_htxToken);
+        BSC_TXR_Token = ERC20Upgradeable(_trxToken);
         dexRouter = IDexRouter(_dexRouter);
         one_time_dividend = _one_time_dividend * 1e18;
         deployTime = block.timestamp;
+        paused = false;
     }
 
     modifier whenNotPaused() {
@@ -69,53 +78,40 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
     }
 
     function bindUser(address _referrer) external nonReentrant whenNotPaused {
-        // 用户的推荐人必须是有效地址, 不能是0地址
         require(_referrer != address(0), "Cannot bind a referrer that is the zero address");
-        // 用户的推荐人不能是自己
         require(_referrer != msg.sender, "Cannot refer yourself");
-        // 用户不能重复绑定上级
         require(!users[msg.sender].isBound, "Already bound to a referrer");
         users[msg.sender].referrer = _referrer;
         users[msg.sender].isBound = true;
-        // 设置用户的层级关系, 把当前用户添加到他的上级的referrals数组中
         users[_referrer].referrals.push(msg.sender);
 
         emit UserBound(msg.sender, _referrer);
     }
 
     function deposit(uint256 amount) external nonReentrant whenNotPaused {
-        // 用户需要通过绑定操作来绑定上级用户, 且只允许绑定一次, 否则不能进行入金操作
         require(users[msg.sender].isBound, "Must be bound to a referrer");
         require(amount == DEPOSIT_AMOUNT, "Deposit amount must be equal to 100 USDT");
 
-        // 检查用户是否满足复投条件
         if (users[msg.sender].depositCount > 0) {
             uint256 requiredReward = users[msg.sender].depositCount * MAX_TOTAL_REWARD_PER_DEPOSIT;
             require(users[msg.sender].totalReward >= requiredReward, "Insufficient total reward for re-deposit");
         }
 
-        // 增加投注次数
         users[msg.sender].depositCount++;
 
-        // Transfer 100 USDT from user to contract
         require(BSC_USDT_Token.transferFrom(msg.sender, address(this), DEPOSIT_AMOUNT), "Transfer failed");
 
         users[msg.sender].hasDeposited = true;
-
-        // 用户入金成功, 计算分红开始时间 用户入金往后推迟1天。
         users[msg.sender].lastUpdateTime = getDividendStartTime();
 
         emit Deposit(msg.sender, DEPOSIT_AMOUNT, users[msg.sender].depositCount);
 
-        // 累加USDT用于兑换
         accumulatedUSDTForSwap += SWAP_USDT_TO_HTX_AMOUNT;
 
-        // 检查是否达到兑换阈值
         if (accumulatedUSDTForSwap >= SWAP_THRESHOLD) {
             swap_USDT_To_HTX();
         }
 
-        // 用户成功入金100usdt, 直接给用户打 one_time_dividend 数量的HTX Token
         require(BSC_HTX_Token.transfer(msg.sender, one_time_dividend), "One time dividend transfer failed");
 
         distributeBonuses(msg.sender);
@@ -130,10 +126,7 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
 
         require(currentTime > lastUpdateTime, "No dividends to claim yet");
 
-        // 计算用户开始计算分红那天的索引
         uint256 startIndex = (lastUpdateTime - deployTime) / 1 days;
-
-        // 计算提现当日的日期索引
         uint256 endIndex = (currentTime - deployTime) / 1 days;
 
         require(startIndex < everyDayDividendAmountArr.length, "There is no dividend to distribute");
@@ -147,10 +140,8 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
         require(totalDividends > 0, "No dividends to claim");
         user.lastUpdateTime = currentTime;
 
-        // 给用户转账
         require(BSC_HTX_Token.transfer(msg.sender, totalDividends * user.depositCount), "Dividend transfer failed");
 
-        // 尝试执行USDT到HTX的兑换
         if (accumulatedUSDTForSwap >= SWAP_THRESHOLD) {
             swap_USDT_To_HTX();
         }
@@ -165,10 +156,7 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
         uint256 currentTime = block.timestamp;
         uint256 lastUpdateTime = user.lastUpdateTime;
 
-        // 计算用户开始计算分红那天的索引
         uint256 startIndex = (lastUpdateTime - deployTime) / 1 days;
-
-        // 计算当日日期索引
         uint256 endIndex = (currentTime - deployTime) / 1 days;
 
         require(startIndex < everyDayDividendAmountArr.length, "There is no dividend to distribute");
@@ -192,15 +180,10 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
 
         uint256 deadline = block.timestamp + 300; // 5 minutes
 
-        uint256[] memory amounts = dexRouter.swapExactTokensForTokens(
-            accumulatedUSDTForSwap,
-            0, // 设置为0表示接受任何数量的HTX代币，请根据需要调整
-            path,
-            address(this),
-            deadline
-        );
+        uint256[] memory amounts =
+            dexRouter.swapExactTokensForTokens(accumulatedUSDTForSwap, 0, path, address(this), deadline);
 
-        accumulatedUSDTForSwap = 0; // 重置累积的USDT数量
+        accumulatedUSDTForSwap = 0;
 
         emit USDTSwappedToHTX(amounts[0], amounts[1]);
     }
@@ -209,11 +192,7 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
         address current = users[_user].referrer;
         uint8 level = 1;
 
-        // console.log("level:-------------", level);
-        // console.log("current:-------------", current);
-
         while (current != address(0) && level <= 17) {
-            // console.log("enter while loop");
             uint256 rewardAmount = 0;
             uint256 directReferrals = users[current].referrals.length;
 
@@ -226,13 +205,8 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
                 rewardAmount = INDIRECT_REWARD_AMOUNT;
             }
 
-            // console.log("rewardAmount amount ---------", rewardAmount);
-
             if (rewardAmount > 0) {
                 uint256 currentMaxTotalReward;
-
-                // console.log("current-------", current);
-                // console.log("owner-----", owner());
 
                 if (current == owner()) {
                     currentMaxTotalReward = type(uint256).max;
@@ -266,7 +240,6 @@ contract InviteAndDividend is Ownable, ReentrancyGuard {
         }
     }
 
-    // 管理员可以设置每天的分红金额
     function setEveryDayDividendAmount(uint256 _everyDayDividend) external onlyOwner {
         everyDayDividendAmountArr.push(_everyDayDividend * 1e18);
     }
